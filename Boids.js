cd000e933fc729c7294bf62931e8fc4485383848
imFernandes23 <87531519+imFernandes23@users.noreply.github.com>
2023-05-20 15:17:48 -0300

    separation-rule

--- Boids.js
+++ Boids.js
@@ -5,7 +5,9 @@ const abruptCurveForce = 0.7;
 const margin = 30;
 const numBoids = 100;
 const drawTrail = true;
-const tailRange = 100
+const tailRange = 50
+const rangeVision = 50;
+const separationForce = 0.1;
 
 //Resizable Canvas
 
@@ -28,9 +30,10 @@ class Boid{
         this.history = [];
     }
 
-    update() {
+    update(Array) {
         this.addTrace()
         this.moveAndColide()
+        this.separation(Array)
     }
 
     moveAndColide(){
@@ -49,6 +52,33 @@ class Boid{
             this.vy *= -1.1;
         }
         this.y += this.vy;
+    }
+
+    separation(Boids){
+        let sumX = 0, 
+            sumY = 0, 
+            count = 0;
+
+        for(const boid of Boids){
+            let d = getDistance(this.x, this.y, boid.x, boid.y);
+            if(d !== 0 && d < rangeVision){
+                let dx = this.x - boid.x;
+                let dy = this.y - boid.y;
+                let dsq = dx*dx + dy*dy;
+
+                if(dsq > 0){
+                    sumX += dx / dsq;
+                    sumY += dy / dsq;
+                    count++;
+                }
+            }
+        }
+
+        if(count > 0){
+            let angle = Math.atan2(sumY, sumX);
+            this.vx += Math.cos(angle) * separationForce;
+            this.vy += Math.sin(angle) * separationForce;
+        }
     }
 
     draw(canvas){
@@ -91,6 +121,12 @@ function getRandomNUmber(min, max){
     return Math.random() * (max - min) + min;
 }
 
+function getDistance(x1,y1,x2,y2){
+    let dx = x1 - x2;
+    let dy = y1 - y2;
+    return Math.sqrt(dx*dx + dy*dy)
+}
+
 //Adicionar boids ao array
 
 for(let i = 0; i < numBoids; i++){
@@ -105,7 +141,7 @@ function animate(){
     context.clearRect(0, 0, canvas.width, canvas.height);
 
     for(let boid of Boids){
-        boid.update();
+        boid.update(Boids);
         boid.draw(context);
     }
     console.log(Boids)
